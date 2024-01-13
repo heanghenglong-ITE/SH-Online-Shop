860ec847d50619624c82dcb2870ed7c24d94e07a
Henglong <pozzlong1998@gmail.com>
2024-01-13 10:26:24 +0700

    Project Practicum

--- app/src/main/java/com/example/shonlineshop/activity/AboutUsActivity.java
+++ app/src/main/java/com/example/shonlineshop/activity/AboutUsActivity.java
@@ -2,15 +2,36 @@ package com.example.shonlineshop.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.shonlineshop.R;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    private ImageView backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        backButton = findViewById(R.id.back_button_about);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed(){
+        Toast.makeText(this,"Back to Account", Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
+    }
+
 }
